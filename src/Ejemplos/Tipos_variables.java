6bde2e2d0f3eac2243a98a184e31c06d74858265
AngelMasterr <angelduarteprog@gmail.com>
2023-07-03 22:11:59 -0500

    actaul

--- src/Ejemplos/Tipos_variables.java
+++ src/Ejemplos/Tipos_variables.java
@@ -12,5 +12,15 @@ public class Tipos_variables {
 		// convertir un decimal a entero
 		int newentero = (int) decimal;
 		System.out.println(newentero);
+		
+		// Int solo soporta 32 bits, si el dato es demasiado grande se usa long: 64 bits
+		// Se debe introducir la letra L al final del numero para indicar que es long
+		long numero_1 = 1222222222222222222L;
+		System.out.println(numero_1);
+		// numeros pequeños
+		short numero_pequeño = 13555;
+		byte numero_mas_pequeño = 15;
+		System.out.println(numero_pequeño+"  "+numero_mas_pequeño);
+		
 	}
 }
